06b736144e211bca6132447e3369e50ddd6d0d5a
ccibg <57514217+ccibg@users.noreply.github.com>
2023-09-21 11:13:32 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://agit.ai/bicg/2023/raw/branch/ai/test/01.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
